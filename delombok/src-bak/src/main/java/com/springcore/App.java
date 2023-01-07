d49f7d278155cbe42b3a2af4fe316882c9955f8c
Sanjeev <msanjeev.200@gmail.com>
2023-01-07 15:12:15 +0530

    Injecting Reference

--- delombok/src-bak/src/main/java/com/springcore/App.java
+++ delombok/src-bak/src/main/java/com/springcore/App.java
@@ -7,9 +7,11 @@ public class App
 {
         public static void main( String[] args )
     {
+
        ApplicationContext context= new ClassPathXmlApplicationContext("config.xml");
        Student student= (Student) context.getBean("student1");
-       System.out.println(student);
+        System.out.println(student.getStudentId());
+        System.out.println(student);
 
     }
 }
--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -1,16 +1,25 @@
 package com.springcore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
+
 public class App
 {
+	private static final Logger log=LoggerFactory.getLogger(App.class);
         public static void main( String[] args )
     {
+       
        ApplicationContext context= new ClassPathXmlApplicationContext("config.xml");
        Student student= (Student) context.getBean("student1");
-        System.out.println(student.getStudentId());
+       System.out.println(student.getStudentId());
        System.out.println(student);
-
+       log.info("Application Started");
+       log.error("error");
+       log.debug("debug");
+       
     }
 }
